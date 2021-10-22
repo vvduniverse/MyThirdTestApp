bd3af93d2a5a963341f7466b6c4620437560bac6
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-22 10:01:00 +0300

    Initial Commit

---
+++ MyThirdTestApp/MyThirdTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyThirdTestAppApp.swift
+//  MyThirdTestApp
+//
+//  Created by Vahtee Boo on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyThirdTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
